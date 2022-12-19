b84903061aa443e51428f10b9382e92456601886
pierre scheibling <pierre.scheibling@live.fr>
2022-12-19 19:29:25 +0100

    function memory

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,9 +2,14 @@ import { motion } from "framer-motion";
 //Styled
 import styled from "styled-components";
 
-function Card(item) {
+function Card({ item, id, handleClick }) {
+  const itemClass = item.stat ? "active" + item.stat : "";
+
   return (
-    <CardItem>
+    <CardItem
+      className={"CardItem" + itemClass}
+      onClick={() => handleClick(id)}
+    >
       <img src={item.img} alt="card" />
     </CardItem>
   );
@@ -15,8 +20,50 @@ const CardItem = styled(motion.div)`
   justify-content: center;
   align-items: center;
   background: white;
+  transform: rotateY(180deg);
+  animation: 2s hideCard linear;
+  transition: transform 0.5s;
+
+  @keyframes hideCard {
+    0%,
+    70% {
+      transform: rotateY(0deg);
+    }
+    100% {
+      transform: rotateY(180deg);
+    }
+  }
+
   img {
-    width: 30%;
+    width: 45%;
+    transform: scale(0);
+    animation: 2s hideImage linear;
+    transition: transform 0.5s;
+  }
+
+  @keyframes hideImage {
+    0%,
+    70% {
+      transform: scale(1);
+    }
+    100% {
+      transform: scale(0);
+    }
+  }
+
+  .card.active {
+    transform: rotateY(0);
+  }
+  .card.active {
+    img {
+      transform: scale(1);
+    }
+  }
+  .card.wrong {
+    background-color: red;
+  }
+  .card.correct {
+    background-color: green;
   }
 `;
 
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -7,16 +7,16 @@ import styled from "styled-components";
 function Cards() {
   const [items, setItems] = useState(
     [
-      { id: 1, img: "/img/stack/HTML.png", stat: "" },
-      { id: 1, img: "/img/stack/HTML.png", stat: "" },
+      { id: 1, img: "src/img/stack/HTML.png", stat: "active" },
+      { id: 1, img: "src/img/stack/HTML.png", stat: "" },
       { id: 2, img: "src/img/stack/CSS.png", stat: "" },
       { id: 2, img: "src/img/stack/CSS.png", stat: "" },
-      { id: 3, img: "src/img/stack/JS.png", stat: "" },
+      { id: 3, img: "src/img/stack/JS.png", stat: "correct" },
       { id: 3, img: "src/img/stack/JS.png", stat: "" },
       { id: 4, img: "src/img/stack/React.png", stat: "" },
       { id: 4, img: "src/img/stack/React.png", stat: "" },
       { id: 5, img: "src/img/stack/ROR.png", stat: "" },
-      { id: 5, img: "src/img/stack/ROR.png", stat: "" },
+      { id: 5, img: "src/img/stack/ROR.png", stat: "wrong" },
       { id: 6, img: "src/img/stack/Figma.png", stat: "" },
       { id: 6, img: "src/img/stack/Figma.png", stat: "" },
       { id: 7, img: "src/img/stack/Github.png", stat: "" },
@@ -25,10 +25,17 @@ function Cards() {
       { id: 8, img: "src/img/stack/API.png", stat: "" },
     ].sort(() => Math.random() - 0.5)
   );
+
+  const [prev, setPrev] = useState(-1);
+
+  function handleClick(id) {
+    alert(id);
+  }
+
   return (
     <GameContainer>
       {items.map((item, index) => (
-        <Card key={index} item={item} id={index} />
+        <Card key={index} item={item} id={index} handleClick={handleClick} />
       ))}
     </GameContainer>
   );
